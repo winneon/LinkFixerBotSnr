4c089ea8f9cd7d44d322e2ce525c38888c96f0bb
WinneonSword <winneonsword@gmail.com>
2013-09-18 17:55:41 -0500

    Initial commit. ( v1.0 )

---
+++ LFB/__init__.py
@@ -0,0 +1,83 @@
+#--------------------#
+#   LinkFixerBotSnr  #
+# By /u/WinneonSword #
+#--------------------#
+
+import praw, time, sys, os, json, re
+from warnings import filterwarnings
+
+filterwarnings("ignore", category = DeprecationWarning)
+filterwarnings("ignore", category = ResourceWarning)
+
+def loadConfig():
+	config = json.loads(open('config.json').read())
+	return config
+
+config = loadConfig()
+version = "v1.0"
+
+userAgent = (
+			"/u/WinneonSword's very own /u/LinkFixerBotSnr," + version +
+			"For more info: http://github.com/WinneonSword/LinkFixerBotSnr ( Coming soon! )"
+)
+
+cache = []
+
+rFind = re.compile(' r/[A-Za-z0-9]+')
+uFind = re.compile(' u/[A-Za-z0-9]+')
+
+def handleRateLimit(func, *args):
+	while True:
+		try:
+			func(*args)
+			break
+		except praw.errors.RateLimitExceeded as error:
+			print("\tRate Limit exceeded! Sleeping for %d seconds to comply with the Reddit API..." % error.sleep_time)
+			time.sleep(error.sleep_time)
+
+def checkComment(comment):
+	text = comment.body
+	broken = set(re.findall(rFind, text))
+	broken.union( set(re.findall(uFind, text)) )
+	condition = False
+	if broken:
+		condition = True
+	return condition, broken
+
+def postComment(comment, text):
+	print("\tFound valid comment at comment id '" + comment.id + "'! Fixing broken link...")
+	message = ''
+	for char in text:
+		message += "/" + char[1:] + "\n"
+	reply = (
+				"" + message + "\n\n"
+				"*****" + "\n\n"
+				"^This ^bot ^is ^a ^fill-in ^for ^/u/LinkFixerBot2 ^until ^it ^gets ^back ^up ^and ^running. ^If ^you ^have ^any ^problems, ^then ^feel ^free ^to ^contact ^/u/WinneonSword."
+	)
+	handleRateLimit(comment.reply, reply)
+	print("\tComment posted! Fixed link: " + message)
+
+def main():
+	username = config['reddit']['username']
+	password = config['reddit']['password']
+	print("[ wsLFB ] - Attempting to connect & login to Reddit...")
+	try:
+		r = praw.Reddit(user_agent = userAgent)
+		r.login(username, password)
+		print("\tSuccessfully connected & logged in to Reddit!")
+	except:
+		print("\tCould not connect to Reddit. Check reddit.com or your config for errors.")
+		sys.exit()
+	try:
+		while True:
+			print("[ wsLFB ] - Fetching new comments...")
+			comments = r.get_comments('all', limit = 500)
+			for c in comments:
+				botMet, text = checkComment(c)
+				if botMet:
+					print("\tFound valid comment at comment id '" + c.id + "'! Fixing broken link...")
+					postComment(c, text)
+			print("\tFinished checking comments! Sleeping for 30 seconds...")
+			time.sleep(30)
+	except KeyboardInterrupt:
+		print("[ wsLFB ] - Stopped LinkFixerBotSnr!")
---
+++ run.py
@@ -0,0 +1,26 @@
+import praw, time, sys, os, json
+
+defaultConfig = {
+	'reddit': {
+		'username': '',
+		'password': '',
+		},
+}
+
+def writeConfig(conf):
+	config = json.dumps(conf, indent = 4, sort_keys = True)
+	with open('config.json', 'w') as f:
+		f.write(config)
+
+if __name__ == "__main__":
+	if not os.path.isfile('config.json'):
+		writeConfig(defaultConfig)
+		print("[ wsLFB ] - Created default configuration. Please edit the values before you start this again.")
+	elif 'updateconf' in sys.argv:
+		with open('config.json', 'r') as f:
+			config = json.loads(f.read())
+		defaultConfig.update(config)
+		writeConfig(defaultConfig)
+	else:
+		import LFB
+		LFB.main()
